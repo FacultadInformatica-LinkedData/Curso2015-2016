1a05416aac441bb807f59f9d07a72933fa2577b6
Julio Dominguez <juliodominguezpuerto@gmail.com>
2015-10-17 22:33:56 +0200

    actualizacion

--- Assignment3/Ejercicio06 t110120.java
+++ Assignment3/Ejercicio06 t110120.java
@@ -2,77 +2,76 @@ package ontologyapi;
 
 import java.io.InputStream;
 
-import com.hp.hpl.jena.ontology.Individual;
-import com.hp.hpl.jena.ontology.OntClass;
-import com.hp.hpl.jena.ontology.OntModel;
-import com.hp.hpl.jena.ontology.OntModelSpec;
-import com.hp.hpl.jena.rdf.model.ModelFactory;
-import com.hp.hpl.jena.rdf.model.Property;
-import com.hp.hpl.jena.util.FileManager;
-import com.hp.hpl.jena.vocabulary.VCARD;
+import org.apache.jena.ontology.Individual;
+import org.apache.jena.ontology.OntClass;
+import org.apache.jena.ontology.OntModel;
+import org.apache.jena.ontology.OntModelSpec;
+import org.apache.jena.rdf.model.ModelFactory;
+import org.apache.jena.rdf.model.Property;
+import org.apache.jena.util.FileManager;
+import org.apache.jena.vocabulary.VCARD;
 
 /**
  * Task 06: Modifying ontologies (RDFs)
- * @author Jonathan Rincon Calixto
-
+ * @author Álvaro Toledano Martín
+ * 
  *
  */
-public class Task06_Jonathan_Rincon
+public class Task06
 {
 	public static String ns = "http://somewhere#";
-	public static String foafNS = "http://xmlns.com/foaf/0.1";
+	public static String foafNS = "http://xmlns.com/foaf/0.1/";
 	public static String foafEmailURI = foafNS+"email";
 	public static String foafKnowsURI = foafNS+"knows";
 	public static String stringTypeURI = "http://www.w3.org/2001/XMLSchema#string";
 	
-
 	public static void main(String args[])
 	{
 		String filename = "example5.rdf";
 		
-
 		// Create an empty model
 		OntModel model = ModelFactory.createOntologyModel(OntModelSpec.RDFS_MEM);
 		
-
 		// Use the FileManager to find the input file
 		InputStream in = FileManager.get().open(filename);
 	
-
 		if (in == null)
 			throw new IllegalArgumentException("File: "+filename+" not found");
 	
-
 		// Read the RDF/XML file
 		model.read(in, null);
 		
 		// Create a new class named "Researcher"
 		OntClass researcher = model.createClass(ns+"Researcher");
 		
-
 		// ** TASK 6.1: Create a new class named "University" **
-		OntClass university = model.createClass(ns+"University");
+		
+		OntClass universidad = model.createClass(ns+"University");
 		
 		// ** TASK 6.2: Add "Researcher" as a subclass of "Person" **
-		model.getOntClass(ns+"Person").addSubClass(university);
+		
+		OntClass persona = model.createClass(ns+"Person");
+		persona.addSubClass(researcher);
 		
 		// ** TASK 6.3: Create a new property named "worksIn" **
+		
 		Property worksIn = model.createProperty(ns+"WorksIn");
 		
 		// ** TASK 6.4: Create a new individual of Researcher named "Jane Smith" **
-		Individual janeSmith = researcher.createIndividual(ns+"JaneSmith");
+		
+		Individual jane = model.createIndividual(ns+"JaneSmith",researcher);
 		
 		// ** TASK 6.5: Add to the individual JaneSmith the fullName, given and family names **
-		janeSmith.addProperty(VCARD.FN, "JaneSmith");
-		janeSmith.addProperty(VCARD.Given, "Jane");
-		janeSmith.addProperty(VCARD.Family, "Smith");
+		
+		jane.addProperty(VCARD.FN,"Jane Smith");
+		jane.addProperty(VCARD.Given,"Jane");
+		jane.addProperty(VCARD.Family,"Smith");
 		
 		// ** TASK 6.6: Add UPM as the university where John Smith works **
-
-		Individual johnSmith = model.getIndividual(ns+"JaneSmith");
-		johnSmith.addProperty(worksIn, university.createIndividual(ns+"UPM"));
 		
+		Individual johnSmith = model.getIndividual(ns+"JohnSmith");
+		Individual upm= universidad.createIndividual(ns+"UPM");
+		johnSmith.addProperty(worksIn, upm);
 		model.write(System.out, "RDF/XML-ABBREV");
-		
 	}
 }
